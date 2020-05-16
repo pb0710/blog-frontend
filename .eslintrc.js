6ff08ce24fcd4363ef07a1d66ac871b678b2b1bf
潘博 <1350210100@qq.com>
2020-05-16 10:02:52 +0800

    Add: git-prev-commit hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,29 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true
+	env: {
+		browser: true,
+		es6: true
 	},
-	"extends": [
+	extends: [
 		// "eslint:recommended",
-		"plugin:react/recommended",
+		'plugin:react/recommended'
 	],
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly'
 	},
-	"parserOptions": {
-		"ecmaFeatures": {
-			"jsx": true
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
 		},
-		"ecmaVersion": 2019,
-		"sourceType": "module"
+		ecmaVersion: 2019,
+		sourceType: 'module'
 	},
-	"plugins": [
-		"react",
-		"react-hooks"
-	],
-	"parser": "babel-eslint",
-	// "rules": {
-	// 	"strict": 0
-	// },
-	"rules": {
-		"react/prop-types": 0,
-		"react-hooks/rules-of-hooks": "error",
-		"quotes": [ "warn", "single" ],
-		"semi": ["warn", "never"]
+	plugins: ['react', 'react-hooks'],
+	parser: 'babel-eslint',
+	rules: {
+		'react/prop-types': 0,
+		'react-hooks/rules-of-hooks': 'error',
+		quotes: ['warn', 'single'],
+		semi: ['warn', 'never']
 	}
-};
+}
