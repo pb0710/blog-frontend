2f4805b4c33188237e9d945422a4e1adfb63a24f
潘博 <1350210100@qq.com>
2021-01-06 14:25:30 +0800

    chore: add deploy scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,43 @@
+const archiver = require('archiver')
+const fs = require('fs')
+const path = require('path')
+const scp = require('node-scp')
+const packageJson = require('../package.json')
+const { serverConfig } = require('./configs')
+
+const currentVersion = packageJson.version
+const fileName = `build.${currentVersion}.zip`
+const packagePath = path.join(process.cwd(), `releases`, fileName)
+
+async function scpPackage() {
+	try {
+		const client = await scp(serverConfig)
+		await client.uploadFile(packagePath, path.join('/data', fileName))
+		client.close() // remember to close connection after you finish
+	} catch (err) {
+		throw err
+	}
+}
+
+function deletePackage() {
+	fs.unlink(packagePath, err => {
+		if (err) console.log(`${packagePath} - deleted errors ${err}`)
+	})
+}
+
+const archive = archiver('zip', {
+	zlib: { level: 9 }
+})
+
+archive.on('error', err => {
+	throw err
+})
+archive.on('end', () => {
+	console.log('END - Data has been drained')
+	scpPackage().then(deletePackage).catch(console.error)
+})
+
+const output = fs.createWriteStream(packagePath)
+archive.pipe(output)
+archive.directory('build/')
+archive.finalize()
